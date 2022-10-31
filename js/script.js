0b44124d7ad470b302f29ba708ea2dc6f79e4cb4
ania221B <abuwaj@gmail.com>
2022-10-31 02:06:24 +0100

    overlay added+button positioned when nav open

--- js/script.js
+++ js/script.js
@@ -8,8 +8,10 @@ navBtn.addEventListener('click', _ => {
   if (visible === 'false') {
     nav.setAttribute('data-visible', true)
     navBtn.setAttribute('aria-expanded', true)
+    document.body.classList.add('menu-is-open')
   } else {
     nav.setAttribute('data-visible', false)
     navBtn.setAttribute('aria-expanded', false)
+    document.body.classList.remove('menu-is-open')
   }
 })
